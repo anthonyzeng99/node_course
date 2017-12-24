d08fab14c2d57c7b51c1c4efe4c1cefb9c53cdf4
Anthony Zeng <anthonyzeng99@gmail.com>
2017-12-24 17:47:18 -0500

    end of lesson 34

--- 03weather_app/app.js
+++ 03weather_app/app.js
@@ -1,32 +1,40 @@
+//
+// const yargs = require('yargs');
+//
+// const geocode = (require('./geocode/geocode.js'))
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
 const request = require('request');
-const yargs = require('yargs');
-
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-    .help()
-    .alias('help', 'h')
-    .argv;
-
-var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+  url: `https://api.darksky.net/forecast/API_KEY/40.6137658,-74.0153231`,
   json: true
 }, (error, response, body) => {
-    if (error) {
-      console.log('Unable to connect to Google Maps API');
-    } else if (body.status === 'ZERO_RESULTS') {
-      console.log('No results found');
-    } else if (body.status === 'OK'){
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log('Unable to connect to Dark Sky API');
+  } else if (response.statusCode === 400){
+    console.log('No weather result found')
+  } else if (response.statusCode === 200)
+    console.log(`Temperature: ${body.currently.temperature}`);
   }
-})
+);
