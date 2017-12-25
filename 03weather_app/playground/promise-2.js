97724f88ab4bc830d5c5fc06f466c2fbf352d6b3
Anthony Zeng <anthonyzeng99@gmail.com>
2017-12-24 20:27:22 -0500

    end of lesson 37

---
+++ 03weather_app/playground/promise-2.js
@@ -0,0 +1,28 @@
+var request = require('request');
+
+var geocodeAddress = (address) => {
+  return new Promise((resolve, reject) => {
+    request({
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+      json: true
+    }, (error, response, body) => {
+      if (error) {
+        reject('Unable to connect to Google Maps API');
+      } else if (body.status === 'ZERO_RESULTS') {
+        reject('No address results found');
+      } else if (body.status === 'OK'){
+        resolve({
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng
+        });
+      }
+    });
+  });
+}
+
+geocodeAddress('19146').then((result) => {
+  console.log(JSON.stringify(result, undefined, 2));
+}, (errorMessage) => {
+  console.log(errorMessage);
+});
