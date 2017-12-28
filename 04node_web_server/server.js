de9c9963bb86a09bca482df10706bd94d8f71731
Anthony Zeng <anthonyzeng99@gmail.com>
2017-12-27 23:49:25 -0500

    end of lesson 48

--- 04node_web_server/server.js
+++ 04node_web_server/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n'), () => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  };
+  next();
+});
+
+app.use((req, res, next) => {
+  // res.render('maintenance.hbs');
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -28,12 +48,18 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Projects page',
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Bad Request'
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
