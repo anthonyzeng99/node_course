491a409b41f90d721f7c293e9913178c850c2499
Anthony Zeng <anthonyzeng99@gmail.com>
2018-01-08 11:40:09 -0500

    end of lesson 53

--- 05node_tests/utils/utils.test.js
+++ 05node_tests/utils/utils.test.js
@@ -1,15 +1,42 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-  if (res != 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res)
+    .toExist()
+    .toBeA('number')
+    .toBe(44);
 });
 
 it('should square number', () => {
   var res = utils.square(5);
-  if (res != 25) {
-    throw new Error(`Expected 25, but got ${res}`);
-  }
+  expect(res)
+    .toExist()
+    .toBeA('number')
+    .toBe(25);
 });
+
+// should verify first and last names are set
+it ('should verify first names are set', () => {
+  var res = utils.setName({}, 'Anthony Zeng');
+
+  expect(res)
+    .toExist()
+    .toBeA('object')
+    .toInclude({
+      firstName: 'Anthony',
+      lastName: 'Zeng'
+    })
+})
+
+
+// it('should expect some values', () => {
+//   expect({
+//     name: 'Anthony',
+//     age: 25,
+//     location: 'New York'
+//   }).toInclude({
+//     age: 23
+//   });
+// });
