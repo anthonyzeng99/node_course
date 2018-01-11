c3ba9365fbc8ecfba7adcf092bcfae12856aac33
Anthony Zeng <anthonyzeng99@gmail.com>
2018-01-11 14:46:48 -0500

    end of lesson 55

--- 05node_tests/utils/utils.test.js
+++ 05node_tests/utils/utils.test.js
@@ -9,12 +9,32 @@ it('should add two numbers', () => {
     .toBe(44);
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum)
+      .toExist()
+      .toBeA('number')
+      .toBe(7)
+    done();
+  });
+});
+
 it('should square number', () => {
   var res = utils.square(5);
   expect(res)
     .toExist()
     .toBeA('number')
     .toBe(25);
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(5, (square) => {
+    expect(square)
+      .toExist()
+      .toBeA('number')
+      .toBe(25)
+    done();
+  });
 });
 
 // should verify first and last names are set
@@ -27,8 +47,8 @@ it ('should verify first names are set', () => {
     .toInclude({
       firstName: 'Anthony',
       lastName: 'Zeng'
-    })
-})
+    });
+});
 
 
 // it('should expect some values', () => {
