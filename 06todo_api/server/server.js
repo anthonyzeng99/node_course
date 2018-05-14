5dada0615fe3cec99f67633119bd7fc6b9b6a9a9
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-14 14:13:28 -0400

    end of lesson 71

--- 06todo_api/server/server.js
+++ 06todo_api/server/server.js
@@ -5,23 +5,45 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: true
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 var newTodo = new Todo({
-  text: 'Buy stuff',
-  completed: true,
-  completedAt: 123
+  text: "(hello) => {}"
 });
 
-newTodo.save().then((result) => {
+// newTodo.save().then((result) => {
+//   console.log(result);
+// }, (error) => {
+//   console.log(error);
+// });
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    time: true,
+    minlength: 1
+  }
+});
+
+var newUser = new User({
+  email: "anthonyzeng99@gmail.com"
+});
+
+newUser.save().then((result) => {
   console.log(result);
 }, (error) => {
   console.log(error);
