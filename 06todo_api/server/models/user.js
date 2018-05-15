ad367b9e29aa492ee93b302320de5879edbfd5ba
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-15 14:05:16 -0400

    finished lesson 88

--- 06todo_api/server/models/user.js
+++ 06todo_api/server/models/user.js
@@ -1,12 +1,33 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const validator = require('validator');
 
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     time: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 // var newUser = new User({
