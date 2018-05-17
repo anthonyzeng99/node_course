bdbe7866f29705844045f34cbbdb4c6dfdae152a
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-17 14:20:44 -0400

    finished lesson 90

--- 06todo_api/server/server.js
+++ 06todo_api/server/server.js
@@ -99,9 +99,11 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  user.save().then((user) => {
-    res.send(user);
-  }).catch((error) => res.status(400).send(error))
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((error) => res.status(400).send(error));
 });
 
 app.listen(port, () => {
