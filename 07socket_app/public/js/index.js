3bf9e4d71a3257fd81cd61e2b223a6da59f5df46
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-24 12:46:50 -0400

    finished lesson 110

--- 07socket_app/public/js/index.js
+++ 07socket_app/public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'James',
-    text: 'Ice cream'
-  });
 });
 
 socket.on('disconnect', function () {
