0182330515b77e688beb4fef6fe69a91e5b7469f
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-24 14:29:26 -0400

    finished lesson 113

--- 07socket_app/server/server.js
+++ 07socket_app/server/server.js
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
 
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user Join'))
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
