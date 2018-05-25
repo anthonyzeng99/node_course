b30f1cfeb0ef816852b08351b6e694f82d1a69ac
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-25 13:00:41 -0400

    finished lesson 133

--- 08async_await/currency-convert.js
+++ 08async_await/currency-convert.js
@@ -4,16 +4,29 @@ const axios = require('axios');
 //20 USD is worth 27.2 CAD You can spend these in the following countries: Canada
 
 const getExchangeRate = async (from, to) => {
-  const response = await axios.get('http://data.fixer.io/api/latest?access_key=bb270f5f971f180e1ec579063536b6b4');
-  const euro = 1 / response.data.rates[from];
-  const rate = euro * response.data.rates[to];
-  return rate;
+  try {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=bb270f5f971f180e1ec579063536b6b4');
+    const euro = 1 / response.data.rates[from];
+    const rate = euro * response.data.rates[to];
+
+    if (isNaN(rate)) {
+      throw new Error();
+    }
+
+    return rate;
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}`);
+  }
 };
 
 const getCountries = async (currencyCode) => {
-  const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-  const countries = response.data.map((country) => country.name);
-  return countries;
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+    const countries = response.data.map((country) => country.name);
+    return countries;
+  } catch (e) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`)
+  }
 };
 
 const convertCurrency = async (from, to, amount) => {
@@ -24,6 +37,8 @@ const convertCurrency = async (from, to, amount) => {
   return `${amount} ${from} is worth ${convertedAmount} ${to} You can spend these in the follow countries: ${countries}`;
 }
 
-convertCurrency('USD', 'GBP', 1).then((response) => {
+convertCurrency('USD', 'EUR', 1).then((response) => {
   console.log(response);
-}).catch((e) => console.log(e));
+}).catch((e) => console.log(e.message));
+
+const add = async (a, b) => a + b + c;
